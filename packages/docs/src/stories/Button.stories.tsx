27958c6dfc0e2db149b03610c388f8d8e2e9236d
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-12-18 18:01:59 -0300

    feat: Componente: Button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,55 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { ArrowRight } from 'phosphor-react'
+
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+const meta: Meta<ButtonProps> = {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new',
+  },
+}
+
+export const Tertiary: Story = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WithIcon: Story = {
+  args: {
+    children: (
+      <>
+        Próximo passo
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+}
