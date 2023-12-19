47efe3702b6f4a19a6d49a2db883e6f505abb5a0
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-12-18 21:40:42 -0300

    feat: Componente: Checkbox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Box, Text, Checkbox, CheckboxProps } from '@ignite-ui/react'
+
+const meta: Meta<CheckboxProps> = {
+  title: 'form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
