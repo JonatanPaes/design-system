c8ac5136b6436496bc18078428fe48b2dc7c430c
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-12-18 22:11:20 -0300

    feat: Componente: MultiStep

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,38 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+
+const meta: Meta<MultiStepProps> = {
+  title: 'form/Multi Step',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {},
+}
+
+export const Full: Story = {
+  args: {
+    currentStep: 3,
+  },
+}
